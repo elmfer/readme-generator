42f6c77aeb5090a9660ef6d4bb17f62abe2ae786
elmfer <elmfer11@outlook.com>
2023-10-18 01:40:08 -0500

    make writeToFile work

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const MarkdownGenerator = require('./utils/generateMarkdown')
+const fs = require('fs');
+const path = require('path');
+const MarkdownGenerator = require('./utils/generateMarkdown');
+
+const OUTPUT_FILE_PATH = "./out";
+const OUTPUT_FILE_NAME = "README.md";
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+  if(!fs.existsSync(OUTPUT_FILE_PATH))
+    fs.mkdirSync(OUTPUT_FILE_PATH);
 
+  fs.writeFileSync(path.join(OUTPUT_FILE_PATH, fileName), data);
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  writeToFile(OUTPUT_FILE_NAME, "test");
+}
 
 // Function call to initialize app
 init();
