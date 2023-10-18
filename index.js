28dc77a17492b3f780eeeaf6b89c306c6cd9cfc7
elmfer <elmfer11@outlook.com>
2023-10-18 01:27:33 -0500

    Added dependancies

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const MarkdownGenerator = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
 function init() {}
